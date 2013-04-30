6aab9351740a5c39334d940565dbda298ab3fcc2
Nikolai Weibull <now@bitwi.se>
2013-04-30 14:36:07 +0200

    Add Inventory-Rake-Tasks-YARD dependency

--- lib/lookout-rake-3.0/version.rb
+++ lib/lookout-rake-3.0/version.rb
@@ -7,6 +7,7 @@ module Lookout::Rake
     def dependencies
       super + Inventory::Dependencies.new{
         development 'inventory-rake', 1, 4, 0
+        development 'inventory-rake-tasks-yard', 1, 3, 0
         development 'yard', 0, 8, 0
         development 'yard-heuristics', 1, 1, 0
         runtime 'rake', 0, 9, 2, :feature => 'rake'
