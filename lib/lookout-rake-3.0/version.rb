4a493fecf5d01f5b6062d01c7246f1b6181f7902
Nikolai Weibull <now@bitwi.se>
2013-04-30 14:38:52 +0200

    Add Lookout dependency

--- lib/lookout-rake-3.0/version.rb
+++ lib/lookout-rake-3.0/version.rb
@@ -8,6 +8,7 @@ module Lookout::Rake
       super + Inventory::Dependencies.new{
         development 'inventory-rake', 1, 4, 0
         development 'inventory-rake-tasks-yard', 1, 3, 0
+        development 'lookout', 3, 0, 0
         development 'yard', 0, 8, 0
         development 'yard-heuristics', 1, 1, 0
         runtime 'rake', 0, 9, 2, :feature => 'rake'
