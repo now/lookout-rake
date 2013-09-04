3edd98b910a9e67ec7e36446770e5a6f1129cd34
Nikolai Weibull <now@bitwi.se>
2013-09-04 14:35:11 +0200

    Update inventory and dependencies

--- Rakefile
+++ Rakefile
@@ -5,11 +5,7 @@ require 'inventory-rake-1.0'
 $:.unshift File.expand_path('../lib', __FILE__)
 require 'lookout-rake-3.0'
 
-Inventory::Rake::Tasks.define Lookout::Rake::Version, :gem => proc{ |_, s|
-  s.author = 'Nikolai Weibull'
-  s.email = 'now@bitwi.se'
-  s.homepage = 'https://github.com/now/lookout-rake'
-}
+Inventory::Rake::Tasks.define Lookout::Rake::Version
 
 Inventory::Rake::Tasks.unless_installing_dependencies do
   Lookout::Rake::Tasks::Test.new
--- lib/lookout-rake-3.0/version.rb
+++ lib/lookout-rake-3.0/version.rb
@@ -3,15 +3,27 @@
 require 'inventory-1.0'
 
 module Lookout::Rake
-  Version = Inventory.new(3, 0, 2){
+  Version = Inventory.new(3, 1, 0){
+    authors{
+      author 'Nikolai Weibull', 'now@disu.se'
+    }
+
+    homepage 'http://disu.se/software/lookout-rake'
+
+    licenses{
+      license 'LGPLv3+',
+              'GNU Lesser General Public License, version 3 or later',
+              'http://www.gnu.org/licenses/'
+    }
+
     def dependencies
       super + Inventory::Dependencies.new{
-        development 'inventory-rake', 1, 4, 0
-        development 'inventory-rake-tasks-yard', 1, 3, 0
+        development 'inventory-rake', 1, 6, 0
+        development 'inventory-rake-tasks-yard', 1, 4, 0
         development 'lookout', 3, 0, 0
         development 'yard', 0, 8, 0
         development 'yard-heuristics', 1, 1, 0
-        runtime 'rake', 0, 9, 2, :feature => 'rake'
+        runtime 'rake', 10, 0, 0, :feature => 'rake'
       }
     end
 
